1c07d6364a7a4158ec18c6ab1ff2d9084aaf87e3
shoeb7558 <83645262+shoeb7558@users.noreply.github.com>
2023-11-12 20:48:44 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,6 @@
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+
+header.style.borderBottom = 'solid 3px #000'
+headerTitle.style.borderBottom = 'solid 3px #000'
+
