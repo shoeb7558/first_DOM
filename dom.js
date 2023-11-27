ccca078ca06570a7c9fba5fcfecf4d19e70c6314
shoeb7558 <83645262+shoeb7558@users.noreply.github.com>
2023-11-27 17:33:31 +0530

    Update_3.1

--- dom.js
+++ dom.js
@@ -1,188 +1,65 @@
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
 
-header.style.borderBottom = 'solid 3px #000'
-headerTitle.style.borderBottom = 'solid 3px #000'
+ document.addEventListener('DOMContentLoaded', function() {
+ 
+ var form = document.getElementById('addForm');
+  var itemList = document.getElementById('items');
+  var filter = document.getElementById('filter');
 
+  // Form submit event
+  form.addEventListener('submit', addItem);
+  // Delete event
+  itemList.addEventListener('click', removeItem);
+  // Filter event
+  filter.addEventListener('keyup', filterItems);
 
-var item = document.getElementsByClassName('list-group-item');
+  function addItem(e) {
+    e.preventDefault();
 
-item[3].style.backgroundColor = 'green';
+    var newItem = document.getElementById('item').value;
+    var newItem1 = document.getElementById('email').value;
 
-for(i=0; i< item.length; i++){
-    item[i].style.fontWeight = 'bold';
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(newItem1));
 
-}
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(deleteBtn);
 
+    var editBtn = document.createElement('button');
+    editBtn.className = 'btn btn-info btn-sm float-right edit';
+    editBtn.appendChild(document.createTextNode('Edit'));
+    li.appendChild(editBtn);
 
-var items = document.getElementsByClassName('list-group-item2');
-items[0].style.fontWeight = 'bold';
-items[0].style.backgroundColor = 'red';
+    itemList.appendChild(li);
 
-item[1].style.backgroundColor = 'green';
+    //addToLocalStorage(newItem, newItem1);
+    addToLocalStorageAndCrudCrud(newItem, newItem1);
+    fetchAndRenderItems();
 
-// Make the 3rd item invisible
-item[2].style.display = 'none';
+    // Reset the form
+    form.reset();
+  }
 
-// Using QuerySelectorAll, change the font color to green for the 2nd item in the item list
-var secondItem = document.querySelectorAll('#items .list-group-item')[1];
-secondItem.style.color = 'green';
-
-// Choose all the odd elements and make their background green using QuerySelectorAll
-var oddItems = document.querySelectorAll('#items .list-group-item:nth-child(odd)');
-for (var i = 0; i < oddItems.length; i++) {
-    oddItems[i].style.backgroundColor = 'green';
-}
-
-
-var newDiv = document.createElement('div');
-
-newDiv.className = 'hello';
-
-newDiv.id = 'hello1';
-
-newDiv.setAttribute('title', 'Hello Div');
-
-var newDivText = document.createTextNode('Hello World');
-
-newDiv.appendChild(newDivText);
-
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
-
-container.insertBefore(newDiv, h1)
-
-
-
-
-var itemList = document.querySelector('#items');
-
-
-var newItem = document.createElement('li');
-
-
-newItem.textContent = 'New Item';
-
-itemList.insertBefore(newItem, itemList.firstChild);
-
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-
-
-// Form submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
-// Filter event
-
-
-// Add item
-function addItem(e){
-  e.preventDefault();
-
-  // Get input value
-  var newItem = document.getElementById('item').value;
-  var newItem1 = document.getElementById('email').value;
-    
-
-  // Create new li element
-  var li = document.createElement('li');
-    
-  // Add class
-  li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
-   li.appendChild(document.createTextNode(newItem1));
-
-  // Create del button element
-  var deleteBtn = document.createElement('button');
-
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
-
-  // Append button to li
-  li.appendChild(deleteBtn);
-
-  // Append li to list
-  itemList.appendChild(li);
-
-
-  var editBtn = document.createElement('button');
-
-  // Add classes to del button
-  editBtn.className = 'btn btn-info btn-sm float-right edit';
-
-  // Append text node
-  editBtn.appendChild(document.createTextNode('Edit'));
-
-  // Append button to li
-  li.appendChild(editBtn);
-
-  // Append li to list
-  itemList.appendChild(li);
-  addToLocalStorage(newItem, newItem1);
-  addToLocalStorageAndCrudCrud(newItem, newItem1);
-fetchAndRenderItems()
-
-  // Reset the form
-  form.reset();
-}
-
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-       var items = document.getElementById('items');
-      
-
-      // Get the index of the item being removed
-      var index = Array.from(items.children).indexOf(li);
-
-      // Remove the item from local storage
-      var existingUsers = JSON.parse(localStorage.getItem('users')) || [];
-      existingUsers.splice(index, 1);
-
-      // Update local storage with the modified array
-      localStorage.setItem('users', JSON.stringify(existingUsers));
-      itemList.removeChild(li);
+  function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+      if (confirm('Are You Sure?')) {
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+      }
     }
   }
-}
 
-// Filter Items
-function filterItems(e){
-  // convert text to lowercase
-  var text = e.target.value.toLowerCase();
-  // Get lis
-  var items = itemList.getElementsByTagName('li');
-  // Convert to an array
-  Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
-      item.style.display = 'block';
-      
-    } else {
-      item.style.display = 'none';
-      
-    }
-  });
-}
-// Filter Items
-function filterItems(e) {
-    // convert text to lowercase
+  function filterItems(e) {
     var text = e.target.value.toLowerCase();
-    // Get lis
     var items = itemList.getElementsByTagName('li');
-    // Convert to an array
     Array.from(items).forEach(function (item1) {
       var itemName = item1.firstChild.textContent;
-      var itemDescription = item1.firstChild.nextSibling.textContent; // Get the item1 text
+      var itemDescription = item1.firstChild.nextSibling.textContent;
       var combinedText = itemName + itemDescription;
-  
+
       if (combinedText.toLowerCase().indexOf(text) != -1) {
         item1.style.display = 'block';
       } else {
@@ -191,62 +68,65 @@ function filterItems(e) {
     });
   }
 
-  
-function addToLocalStorage(name, email) {
-    const user = {
-      name: name,
-      email: email
-    };
-  
-    const existingUsers = JSON.parse(localStorage.getItem('users')) || [];
-    existingUsers.push(user);
-    localStorage.setItem('users', JSON.stringify(existingUsers));
-  }
+  //function addToLocalStorage(name, email) {
+  //  const user = {
+  //    name: name,
+  //    email: email
+  //  };
 
-  
+  //  const existingUsers = JSON.parse(localStorage.getItem('users')) || [];
+   // existingUsers.push(user);
+  //  localStorage.setItem('users', JSON.stringify(existingUsers));
+  //}
 
   function addToLocalStorageAndCrudCrud(name, email) {
     const user = {
       name: name,
       email: email
     };
-  
-    // Add item to local storage
-    
-  
 
-      // Save data to CRUD CRUD API using Axios
-     // const apiUrl = 'https://crudcrud.com/api/c76077076e374296a4538a2665e87c35/your-collection'; // Replace 'your-collection' with your actual collection ID
-    axios.post("https://crudcrud.com/api/c76077076e374296a4538a2665e87c35/uniqueitemlist", user)
-    .then((response) => {
-      console.log('Data saved to CRUD CRUD API successfully.');
-    })
-     .catch((err) => {
-      console.log('error');
-    });
+    // Save data to CRUD CRUD API using Axios
+    axios.post("https://crudcrud.com/api/8ce90492c21a452688f87ff01e5b0db8/uniqueitemlist", user)
+      .then((response) => {
+        console.log('Data saved to CRUD CRUD API successfully.');
+        // Fetch updated data after successful save
+        //fetchAndRenderItems();
+      })
+      .catch((error) => {
+        console.log('Error :', error);
+      });
   }
 
- function fetchAndRenderItems() {
-    axios.get("https://crudcrud.com/api/6b4ef31f91ec4d6d81148e63c5cd79e6/uniqueitemlist")
+  function fetchAndRenderItems() {
+    axios.get("https://crudcrud.com/api/8ce90492c21a452688f87ff01e5b0db8/uniqueitemlist")
       .then((response) => {
         // Clear existing items
-        //itemList.innerHTML = '';
+        itemList.innerHTML = '';
+        
         // Render items
         for (var i = 0; i < response.data.length; i++) {
-          addItem(response.data[i]);
+          var item = response.data[i];
+          var li = document.createElement('li');
+          li.className = 'list-group-item';
+          li.appendChild(document.createTextNode(item.name));
+          li.appendChild(document.createTextNode(item.email));
+  
+          var deleteBtn = document.createElement('button');
+          deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+          deleteBtn.appendChild(document.createTextNode('X'));
+          li.appendChild(deleteBtn);
+  
+          var editBtn = document.createElement('button');
+          editBtn.className = 'btn btn-info btn-sm float-right edit';
+          editBtn.appendChild(document.createTextNode('Edit'));
+          li.appendChild(editBtn);
+  
+          itemList.appendChild(li);
         }
       })
       .catch((error) => {
         console.log('Error fetching data from CRUD CRUD API:', error);
       });
   }
-
-  document.addEventListener('DOMContentLoaded', function () {
-  // Fetch initial data when the page loads
-  fetchAndRenderItems();
+  fetchAndRenderItems()
 });
-
-
-
-
-
